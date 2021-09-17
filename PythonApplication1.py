9638f40284b0de9141c1b1f4e371a463c5c886bf
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2021-09-17 10:19:27 +0300

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,30 @@
+def input_array(n, m, matrixA):
+    for i in range(n):
+        for j in range(m):
+            matrixA[i][j] = int(input())
+
+def print_array(n, m, matrixA):
+    for i in range(n):
+        string = ""
+        for j in range(m):
+            string +=str(matrixA[i][j]) +  " " 
+        print(string)
+        
+
+def build_matrixB(martixA, matrixB):
+    for i in range(len(matrixA)):
+        for j in range(len(matrixB)):
+            for k in range(j, m):
+                matrixB[i][j] += matrixA[j][k]
+
+n = int(input("Input N: "))
+m = int(input("Input M: "))
+
+matrixA = [[0 for i in range(n)] for j in range(m)]
+input_array(n,m,matrixA)
+
+matrixB = [[0 for i in range(n)] for j in range(m)]
+build_matrixB(matrixA, matrixB)
+
+
+
