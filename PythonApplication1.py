7069e41017a86ad73e7fc13544e522d210371151
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2021-09-21 18:06:28 +0300

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,3 +1,4 @@
+import random
 class MyClassException(Exception): pass
 
 def input_array(n, m, matrixA):
@@ -5,6 +6,13 @@ def input_array(n, m, matrixA):
     for i in range(n):
         for j in range(m):
             matrixA[i][j] = int(input())
+
+def input_random_from_A_to_B(n, m, matrixA):
+    A = int(input(" Input A: "))
+    B = int(input(" Input B: "))
+    for i in range(n):
+        for j in range(m):
+            matrixA[i][j] = random.randint(A,B)
 
 def print_array(n, m, matrixA):
     for i in range(n):
@@ -21,22 +29,30 @@ def build_matrixB(matrixA, matrixB):
                 matrixB[i][j] += matrixA[k][j]
 
 while True:
-    try:
-        n = int(input("Input N: "))
-        m = int(input("Input M: "))
+    try:        
+        int_menu = int(input("  Enter '1' if you want to fill in the matrix A yourself\n" + 
+                             "  Enter '2' if you want to fill the matrix A with random numbers from A to B\n" +
+                             "  Enter '3' if you want to exit\n"))
+        if int_menu == 3: break
+        elif (int_menu < 1 or int_menu > 3):
+            print("Please try again")
+            continue
+        else:
+            n = int(input("Input N: "))
+            m = int(input("Input M: "))
+            if (n <= 1 or m <= 1): raise MyClassException
 
-        if (n <= 1 or m <= 1): raise MyClassException
-
-        matrixA = [[0 for i in range(m)] for j in range(n)]
-        input_array(n,m,matrixA)
+            matrixA = [[0 for i in range(m)] for j in range(n)]
+            if int_menu == 1: input_array(n, m, matrixA)
+            else: input_random_from_A_to_B(n, m, matrixA)
 
         matrixB = [[0 for i in range(m)] for j in range(n)]
         build_matrixB(matrixA, matrixB)
 
-        print("\n\n")
+        print("\n\n  MATRIX A")
         print_array(n,m,matrixA)
 
-        print("\n\n")
+        print("\n  RESULT: MATRIX B")
         print_array(n,m,matrixB)
 
         string = input("\n\n If you want to finish enter 'exit': ")
