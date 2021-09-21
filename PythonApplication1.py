9c0c3e1793253901ade985df84e56fee4ecafa22
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2021-09-21 16:32:54 +0300

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,4 +1,7 @@
+class MyClassException(Exception): pass
+
 def input_array(n, m, matrixA):
+    print("Input the matrix of size " + str(n) + "x" + str(m))
     for i in range(n):
         for j in range(m):
             matrixA[i][j] = int(input())
@@ -11,23 +14,37 @@ def print_array(n, m, matrixA):
         print(string)
         
 
-def build_matrixB(martixA, matrixB):
+def build_matrixB(matrixA, matrixB):
     for i in range(len(matrixA)):
-        for j in range(len(matrixB)):
-            for k in range(i, m):
+        for j in range(len(matrixA[i])):
+            for k in range(i, n):
                 matrixB[i][j] += matrixA[k][j]
 
-n = int(input("Input N: "))
-m = int(input("Input M: "))
+while True:
+    try:
+        n = int(input("Input N: "))
+        m = int(input("Input M: "))
 
-matrixA = [[0 for i in range(n)] for j in range(m)]
-input_array(n,m,matrixA)
+        if (n <= 1 or m <= 1): raise MyClassException
 
-matrixB = [[0 for i in range(n)] for j in range(m)]
-build_matrixB(matrixA, matrixB)
+        matrixA = [[0 for i in range(m)] for j in range(n)]
+        input_array(n,m,matrixA)
 
-print("\n\n")
-print_array(n,m,matrixA)
+        matrixB = [[0 for i in range(m)] for j in range(n)]
+        build_matrixB(matrixA, matrixB)
 
-print("\n\n")
-print_array(n,m,matrixB)
+        print("\n\n")
+        print_array(n,m,matrixA)
+
+        print("\n\n")
+        print_array(n,m,matrixB)
+
+        string = input("\n\n If you want to finish enter 'exit': ")
+        if(string == "exit"): break
+
+    except ValueError:
+        print(" ERROR: Variable should be INT\n")
+        continue
+    except MyClassException:
+        print(" ERROR: N and M should be '>' 1\n")
+        continue
