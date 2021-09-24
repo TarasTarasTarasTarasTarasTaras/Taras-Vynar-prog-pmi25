1926cf148b6c06c3ba37f2107539d7575dcad7d6
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2021-09-24 13:26:45 +0300

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,64 +1,88 @@
 import random
 
-def input_array(n, m, matrixA):
-    print("Input the matrix of size " + str(n) + "x" + str(m))
-    for i in range(n):
-        for j in range(m):
-            matrixA[i][j] = int(input())
+def input_array(matrixA):
+    print("\n Input the matrix of size " + str(len(matrixA)) + "x" + str(len(matrixA[0])))
+    while True:
+        try:
+            for i in range(len(matrixA)):
+                for j in range(len(matrixA[i])):
+                    matrixA[i][j] = int(input("Matrix A["+str(i+1)+"]["+str(j+1)+"] = "))
+            break
+        except:
+            print(" ERROR: Value must be INT")
+            continue
+                    
 
-def input_random_from_A_to_B(n, m, matrixA):
-    A = int(input(" Input A: "))
-    B = int(input(" Input B: "))
-    for i in range(n):
-        for j in range(m):
+def input_random_from_A_to_B(matrixA, A, B):
+    for i in range(len(matrixA)):
+        for j in range(len(matrixA[i])):
             matrixA[i][j] = random.randint(A,B)
 
-def print_array(n, m, matrixA):
-    for i in range(n):
+
+def print_array(matrixA):
+    for i in range(len(matrixA)):
         string = ""
-        for j in range(m):
-            string +=str(matrixA[i][j]) +  " " 
+        for j in range(len(matrixA[i])):
+            string += str(matrixA[i][j]) +  " "
         print(string)
+    print('\n')
         
 
 def build_matrixB(matrixA, matrixB):
     for i in range(len(matrixA)):
         for j in range(len(matrixA[i])):
-            for k in range(i, n):
+            for k in range(i, len(matrixA)):
                 matrixB[i][j] += matrixA[k][j]
 
-while True:
-    try:        
-        int_menu = int(input("  Enter '1' if you want to fill in the matrix A yourself\n" + 
-                             "  Enter '2' if you want to fill the matrix A with random numbers from A to B\n" +
-                             "  Enter '3' if you want to exit\n"))
-        if int_menu == 3: break
-        elif (int_menu < 1 or int_menu > 3):
-            print("Please try again")
+
+def menu():
+    while True:
+        str_menu = input("  Enter 'random' if you want to fill the matrix A with random numbers from A to B\n" +
+                         "  Enter 'fill' if you want to fill in the matrix A yourself\n" + 
+                         "  Enter 'exit' if you want to exit\n")
+
+        if str_menu == "exit": 
+            break
+        elif str_menu != "fill" and str_menu != "random":
+            print("\nPlease try again")
             continue
         else:
-            n = int(input("Input N: "))
-            m = int(input("Input M: "))
-            if (n <= 1 or m <= 1):
-                print("N and M must be '>' 1")
-                continue
+            n = m = 0
+            while (n <= 0 or m <= 0):
+                print("\nN and M must be '>' 0")
+                try:
+                    n = int(input("Input N: "))
+                    m = int(input("Input M: "))
+                except: 
+                    print(" ERROR: N and M must be INT")
+                    continue
 
             matrixA = [[0 for i in range(m)] for j in range(n)]
-            if int_menu == 1: input_array(n, m, matrixA)
-            else: input_random_from_A_to_B(n, m, matrixA)
+            if str_menu == "fill": 
+                input_array(matrixA)
+            else:
+                while True:
+                    try:
+                        A = int(input(" Input A: "))
+                        B = int(input(" Input B: "))
+                        break
+                    except: 
+                        print("ERROR: A and B must be INT")
+                        continue
+                if A > B: 
+                    A=A+B; 
+                    B=A-B; 
+                    A=A-B;
+                input_random_from_A_to_B(matrixA, A, B)
 
         matrixB = [[0 for i in range(m)] for j in range(n)]
         build_matrixB(matrixA, matrixB)
 
         print("\n\n  MATRIX A")
-        print_array(n,m,matrixA)
+        print_array(matrixA)
 
-        print("\n  RESULT: MATRIX B")
-        print_array(n,m,matrixB)
+        print("  RESULT: MATRIX B")
+        print_array(matrixB)
 
-        string = input("\n If you want to finish enter 'exit': ")
-        if(string == "exit"): break
 
-    except ValueError:
-        print(" ERROR: Variable must be INT\n")
-        continue
+menu()
