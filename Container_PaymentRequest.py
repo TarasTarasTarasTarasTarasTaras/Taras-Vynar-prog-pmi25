9af27abf28dde10041b7542ec6a435de750633fc
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2021-09-30 10:37:22 +0300

    Add files via upload

---
+++ Container_PaymentRequest.py
@@ -0,0 +1,151 @@
+import json
+from Validation import Validation
+from Payment_Request import PAYMENT_REQUEST
+
+
+class ContainerPAYMENT_REQUEST: 
+    def __init__(self, file_name = "None"):
+        self.__array_of_elements = []
+        self.__file_name = file_name
+        self.__validation = Validation()
+
+
+    def __str__(self):
+        string = ""
+        for payment in self.__array_of_elements:
+            string += str(payment) + "\n" + "-"*30 + "\n"
+        return string
+
+
+    def __repr__(self):
+        return self.__str__()
+
+
+    def read_from_file(self, value = "None"):
+        try:
+            if (self.__validation.validate_file("File name", value)) is True: 
+                file = open(value)
+        except ValueError as message:
+            raise ValueError("Error reading from file: " + str(message))
+        except FileNotFoundError:
+            raise ValueError("  ERROR: File not found")
+        
+        self.__array_of_elements = []
+        filesJSON = json.load(file)
+        number = success = 0
+        for payment_request in filesJSON:
+            number += 1
+            try:
+                payment = PAYMENT_REQUEST(
+                    payment_request.get("ID"),
+                    payment_request.get("Amount"),
+                    payment_request.get("Currency"),
+                    payment_request.get("Email"),
+                    payment_request.get("Transaction ID"),
+                    payment_request.get("Due to date"),
+                    payment_request.get("Request date"))
+            except ValueError as message:
+                print(message)
+                print("Product number " + str(number) + " is defective")
+                continue
+            self.__array_of_elements.append(payment)
+            success += 1
+        print("\n" + str(success) + " objects were successfully read from the file: " + str(value))
+
+
+    def write_to_file(self, value):
+        try:
+            if self.__validation.validate_file("File name", value) is True:
+                file = open(value, mode = "w")
+        except ValueError as message:
+            print("  ERROR: " + str(message))
+            raise ValueError("Error reading from file")
+        except FileNotFoundError:
+            raise ValueError("  ERROR: File not found")
+
+        file.write("[")
+        current_payment = 0
+        for payments in self.__array_of_elements:
+            file.write(payments)
+            current_payment += 1
+            if(current_payment < len(self.__array_of_elements) - 1):
+                file.write(", ")
+        file.write("]")
+        file.close()
+
+
+    def append(self, payment):
+        try:
+            self.__validation.validate_PAYMENT(payment)
+        except ValueError as message:
+            raise ValueError(message + "\nObject not added")
+        
+        self.__array_of_elements.append(payment)
+        if self.__file_name != "None":
+            self.write_to_file(self.__file_name)
+        print("Object added successfully")
+
+
+    def delete(self, ID):
+        try:
+            if self.__validation.validate_ID("ID", ID) is not True:
+                raise ValueError("")
+            index = 0
+            for payment in self.__array_of_elements:
+                if payment.get_id() == str(ID):
+                    self.__array_of_elements.pop(index)
+                    if self.__file_name != "None":
+                        self.write_to_file(self.__file_name)
+                    print("Object deleted successfully")
+                    return
+                index += 1
+            else: 
+                raise ValueError("Container is empty")
+            raise ValueError("Object not found")
+        except ValueError as message:
+            raise ValueError("Item can not be deleted. " + message)
+
+
+    def search_in_container(self, search_string):
+        wanted_payments = ContainerPAYMENT_REQUEST()
+        counter_of_found_elements = 0
+
+        for payment in self.__array_of_elements:
+            for getter in payment.getAll_getters():
+                if str(getattr(payment, getter)()).find(str(search_string)) != -1:
+                    wanted_payments.append(payment)
+                    counter_of_found_elements += 1
+                    break
+
+        print("Payments successfully found: " + str(counter_of_found_elements))
+        return wanted_payments
+
+
+    def sort(self, key):
+        try:
+            self.__validation.validate_is_str("Attribute", key)
+        except ValueError as message:
+            raise ValueError(message + "\nThe container can not be sorted by the following key: " + key)
+        
+        value = key.lower()
+        value = value.replace(' ', '_')
+        value = "get_" + value
+
+        if type(getattr(self.__array_of_elements[0], value)) is str:
+            self.__array_of_elements.sort(key = lambda payment : getattr(payment, value)().lower())
+        else:
+            self.__array_of_elements.sort(key = lambda payment : getattr(payment, value)())
+
+        print("The container was successfully sorted by the attribute: " + key)
+
+
+    def clear(self):
+        if len(self.__array_of_elements) == 0:
+            raise ValueError("Container is empty")
+        while not self.__array_of_elements:
+            self.__array_of_elements.pop()
+
+
+    def set_the_file(self, filename):
+        self.__file_name = filename
+
---
+++ Payment_Request.py
@@ -0,0 +1,51 @@
+from Validation import Validation
+from Validation import ExceptionPayment
+
+class PAYMENT_REQUEST:
+    def __init__(self, ID, amount, currency, payer_email, transaction_id, payment_due_to_date, payment_request_date):
+        self.__validation = Validation()
+        try:
+            if self.__validation.validate_ID("ID", ID)                                    is True:  self.__ID = ID
+            if self.__validation.validate_amount("Amount", amount)                        is True:  self.__amount = amount
+            if self.__validation.validate_currency("Currency", currency)                  is True:  self.__currency = currency
+            if self.__validation.validate_email("Email", payer_email)                     is True:  self.__payer_email = payer_email
+            if self.__validation.validate_transactionID("Transaction ID", transaction_id) is True:  self.__transaction_id = transaction_id
+            if self.__validation.validate_date("Due to date", payment_due_to_date)        is True:  self.__payment_due_to_date = payment_due_to_date 
+            if self.__validation.validate_date("Request date", payment_request_date)      is True:  self.__payment_request_date = payment_request_date
+        except ExceptionPayment as message:
+            raise ValueError(str(message) + "\nError creating object")
+
+
+    def __str__(self):
+        return("ID: " + str(self.__ID) + 
+                "\nAmount: " + self.__amount +
+                "\nCurrency: " + str(self.__currency) + 
+                "\nEmail: " + self.__payer_email + 
+                "\nTransaction ID: " + self.__transaction_id + 
+                "\nDue to date: " + str(self.__payment_due_to_date) + 
+                "\nRequest date: " + str(self.__payment_request_date))
+
+
+    def get_id(self):
+        return self.__ID
+    def get_payer_email(self):
+        return self.__payer_email
+    def get_amount(self):
+        return self.__amount 
+    def get_currency(self):
+        return self.__currency
+    def get_payment_request_date(self):
+        return self.__payment_request_date
+    def get_payment_due_to_date(self):
+        return self.__payment_due_to_date
+    def get_transaction_id(self):
+        return self.__transaction_id
+
+
+    def getAll_getters(self):
+        array_of_getters = []
+        for local_attr in dir(self):
+            if local_attr.startswith("get_") and callable(getattr(self, local_attr)):
+                array_of_getters.append(local_attr)
+        return array_of_getters
+
