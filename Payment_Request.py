c524732252c6d2a1275f7cc4e6bdeefacc058735
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2021-10-08 02:02:31 +0300

    Update Payment_Request.py

--- Payment_Request.py
+++ Payment_Request.py
@@ -1,68 +1,68 @@
-from Validation import Validation
-from Validation import ExceptionPayment
+from Validation import *
+
 
 class PAYMENT_REQUEST:
-    def __init__(self, ID, amount, currency, payer_email, transaction_id, payment_due_to_date, payment_request_date):
+    array_of_names = ["id", "amount", "currency", "payer_email", "transaction_id", "payment_due_to_date", "payment_request_date"]
+
+    def __init__(self, id, amount, currency, payer_email, transaction_id, payment_due_to_date, payment_request_date):
         self.__validation = Validation()
         try:
-            if self.__validation.validate_ID("ID", ID)                                    is True:  self.__ID = ID
-            if self.__validation.validate_amount("Amount", amount)                        is True:  self.__amount = amount
-            if self.__validation.validate_currency("Currency", currency)                  is True:  self.__currency = currency
-            if self.__validation.validate_email("Email", payer_email)                     is True:  self.__payer_email = payer_email
-            if self.__validation.validate_transactionID("Transaction ID", transaction_id) is True:  self.__transaction_id = transaction_id
-            if self.__validation.validate_date("Due to date", payment_due_to_date)        is True:  self.__payment_due_to_date = payment_due_to_date 
-            if self.__validation.validate_date("Request date", payment_request_date)      is True:  self.__payment_request_date = payment_request_date
+            self.set_id(id)
+            self.set_amount(amount)
+            self.set_currency(currency)
+            self.set_payer_email(payer_email)
+            self.set_transaction_id(transaction_id)
+            self.set_payment_due_to_date(payment_due_to_date)
+            self.set_payment_request_date(payment_request_date)
         except ExceptionPayment as message:
             raise ValueError(str(message) + "\nError creating object")
 
 
     def __str__(self):
-        return('"ID": ' + str(self.__ID) +
-               '\n"Amount": ' + str(self.__amount) +
-               '\n"Currency": ' + str(self.__currency) +
-               '\n"Email": ' + str(self.__payer_email) + 
-               '\n"TransactionID": ' + str(self.__transaction_id) + 
-               '\n"Due_to_date": ' + str(self.__payment_due_to_date) +  
-               '\n"Request_date": ' + str(self.__payment_request_date) + '\n')
-
+        string = ""
+        for i in range(len(self.array_of_names)):
+            string += self.array_of_names[i] + ": " + str(self.__getattribute__(self.array_of_names[i])).lower()
+            if i < len(self.array_of_names) - 1: string += "\n"
+        return string
+    
 
     def get_id(self):
-        return self.__ID
+        return self.id
     def get_payer_email(self):
-        return self.__payer_email
+        return self.payer_email
     def get_amount(self):
-        return self.__amount 
+        return self.amount 
     def get_currency(self):
-        return self.__currency
+        return self.currency
     def get_payment_request_date(self):
-        return self.__payment_request_date
+        return self.payment_request_date
     def get_payment_due_to_date(self):
-        return self.__payment_due_to_date
+        return self.payment_due_to_date
     def get_transaction_id(self):
-        return self.__transaction_id
+        return self.transaction_id
 
 
+    @Validation.validate_ID
     def set_id(self, id):
-        if self.__validation.validate_ID("ID", id) is True:
-            self.__ID = id
-    def set_payer_email(self, email):
-        if self.__validation.validate_email("Email", email) is True:
-            self.__payer_email = email
+        self.id = id
+    @Validation.validate_amount
     def set_amount(self, amount):
-        if self.__validation.validate_amount("Amount", amount) is True:
-            self.__amount = amount
+        self.amount = amount
+    @Validation.validate_currency
     def set_currency(self, currency):
-        if self.__validation.validate_currency("Currency", currency) is True:
-            self.__currency = currency
+        self.currency = currency
+    @Validation.validate_email
+    def set_payer_email(self, email):
+        self.payer_email = email
+    @Validation.validate_date
     def set_payment_request_date(self, date):
-        if self.__validation.validate_date("Request date", date) is True:
-            self.__payment_request_date = date
+        self.payment_request_date = date
+    @Validation.validate_date
     def set_payment_due_to_date(self, date):
-        if self.__validation.validate_date("Due to date", date) is True:
-            self.__payment_due_to_date = date
+        self.payment_due_to_date = date
+    @Validation.validate_transactionID
     def set_transaction_id(self, id):
-        if self.__validation.validate_transactionID("Transaction ID", id) is True:
-            self.__transaction_id = id
+        self.transaction_id = id
 
 
     def getAll_getters(self):
