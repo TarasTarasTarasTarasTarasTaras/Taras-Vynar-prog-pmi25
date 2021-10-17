49fd68be466f23dd013f09dcd54e3ebc23139bfa
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2021-10-17 20:09:43 +0300

    Add files via upload

---
+++ Memento.py
@@ -0,0 +1,36 @@
+class Memento():
+    def __init__(self, state):
+        self.__state = state;
+
+    def get_state(self):
+        return self.__state
+
+
+class Caretaker():
+    def __init__(self):
+        self.__snapshots_undo = []
+        self.__snapshots_redo = []
+
+    def add_snapshot_undo(self, snapshot: Memento):
+        self.__snapshots_undo.append(snapshot)
+
+    def add_snapshot_redo(self, snapshot: Memento):
+        self.__snapshots_redo.append(snapshot)
+
+    def get_undo_state(self):
+        return self.__snapshots_undo[-1].get_state()
+
+    def get_redo_state(self):
+        return self.__snapshots_redo[-1].get_state()
+
+    def pop_snapshot_undo(self):
+        return self.__snapshots_undo.pop()
+
+    def pop_snapshot_redo(self):
+        return self.__snapshots_redo.pop()
+
+    def size_snapshot_undo(self):
+        return len(self.__snapshots_undo)
+
+    def size_snapshot_redo(self):
+        return len(self.__snapshots_redo)
