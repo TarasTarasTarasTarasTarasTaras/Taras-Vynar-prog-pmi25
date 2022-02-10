0f106a07948df4bed9a3ddb47f2f8caf32502072
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2022-02-10 11:16:33 +0200

    Delete authorization directory

--- authorization/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('login/', SignInAPI.as_view()),
-    path('logout/', LogOutAPI.as_view()),
-    path('users/', SignUpAPI.as_view()),
-]
